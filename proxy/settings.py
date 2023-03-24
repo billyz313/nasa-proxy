7297105808b0554d76c3f127a36d57e1aadc3d96
Billy Ashmall <billy.ashmall@nasa.gov>
2023-03-24 16:05:25 -0500

    cors

--- proxy/settings.py
+++ proxy/settings.py
@@ -29,18 +29,6 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
-
 
 # Application definition
 
